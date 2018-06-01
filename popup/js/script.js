224e4e21294bffe1af27025e4b9319a9cb5bf774
djmgit <djmdeveloper060796@gmail.com>
2018-06-02 01:15:42 +0530

    Added login method

--- popup/js/script.js
+++ popup/js/script.js
@@ -1,4 +1,30 @@
 var app = angular.module('exApp', []);
 app.controller('exCtrl', function($scope, $http) {
 	
+    $scope.isLoggedIn = function() {
+        if (localStorage.getItem("token") == null) {
+            return false;
+        } else {
+        	$scope.token = localStorage.getItem("token");
+            return true;
+        }
+    }
+
+    $scope.login = function() {
+    	var username = $scope.username;
+    	var password = $scope.password;
+
+    	var dataObj = {"username": username, "password": password};
+
+    	var res = $http.post("https://app.accidentally14.hasura-app.io/api/login", dataObj);
+    	res.then(function(response) {
+    		$scope.token = response.data.data.auth_token;
+    		localStorage.setItem("item", $scope.token)
+    		$scope.fetchNotes();
+    	});
+    }
+
+    $scope.fetchNotes = function() {
+    	console.log("fetching notes")
+    }
 });
