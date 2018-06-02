9dc3a8e2d286a56beb7f30127e1c86b804824322
djmgit <djmdeveloper060796@gmail.com>
2018-06-02 12:25:03 +0530

    Added method to add notes

--- popup/js/script.js
+++ popup/js/script.js
@@ -66,4 +66,40 @@ app.controller('exCtrl', function($scope, $http) {
     		});
     	}
     }
+
+    $scope.createNote = function() {
+        var note_title = $scope.note_title;
+        var note_body = $scope.note_body;
+        var keywords = $scope.note_keywords;
+        var category = $scope.note_category;
+
+        var dataObj = {}
+        if (note_title !== null && note_title !== "") {
+            dataObj["note_title"] = note_title
+        }
+
+        if (note_body !== null && note_body !== "") {
+            dataObj["note_body"] = note_body
+        }
+
+        if (keywords !== null && keywords !== "") {
+            dataObj["keywords"] = keywords
+        }
+
+        if (category !== null && category !== "") {
+            dataObj["category"] = category
+        }
+
+        $scope.token = localStorage.getItem("token");
+
+        var config = {headers:  {
+                    "Authorization": "Bearer " + $scope.token
+                }
+            };
+
+        var res = $http.post("https://app.accidentally14.hasura-app.io/api/add_note", dataObj,  config);
+            res.then(function(response) {
+                console.log(response.data)
+            });
+    }
 });
